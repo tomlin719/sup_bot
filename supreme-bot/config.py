62b04d9299f1fd46d73e2663c9c50dc9d0cba147
tomlin719 <r06922155@ntu.edu.tw>
2020-03-08 14:00:12 +0800

    jj
    jj

---
+++ supreme-bot/config.py
@@ -0,0 +1,11 @@
+keys = {
+        "product_url": "https://www.supremenewyork.com/shop/all",
+        "name": "John Fisher",
+        "email": "email@gmail.com",
+        "phone_number": "6789998212",
+        "street_address": "Forest Hills Dr.",
+        "city": "Timbuktu",
+        "zip_code": "91601",
+        "card_cvv": "666",
+        "card_number": "1228199669918221"
+}
